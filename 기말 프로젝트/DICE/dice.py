8d7d288ff6c691553e251351e766386b51e2b093
Meenki <meenki98@gmail.com>
2020-11-23 20:34:17 +0900

    커밋

---
+++ 기말 프로젝트/DICE/dice.py
@@ -0,0 +1,25 @@
+from pico2d import *
+import gfw
+
+class Dice:
+	def __init__(self, imageName, pos, type):
+        self.time = get_time()
+        self.image = gfw.image.load(RES_DIR + '/' + imageName)
+        self.pos = pos
+        self.fps = 8
+        if fcount == 0:
+            fcount = self.image.w // self.image.h
+
+        self.width = self.image.w // fcount
+        self.height = self.image.h
+        self.fcount = fcount
+
+	def update(self):
+
+
+	def draw(self):
+        elapsed = get_time() - self.time
+        fidx = round(elapsed * self.fps) % self.fcount
+        sx = self.width * fidx
+        size = self.width * self.mag, self.height * self.mag
+        self.image.clip_draw(sx, 0, self.width, self.height, *self.pos, *size)
---
+++ 기말 프로젝트/DICE/main_state.py
@@ -0,0 +1,41 @@
+from pico2d import *
+import gfw
+import dice
+import ui
+import enemy
+
+def enter():
+	gfw.world.init(['UI', 'player', 'enemy'])
+
+def exit():
+	pass
+
+def update():
+	gfw.world.update()
+
+def draw():
+	gfw.world.draw()
+
+def KeyDown(k):
+	if k == SDLK_ESCAPE:
+		gfw.pop()
+
+def Mouse(e):
+	global mousePos
+	if e.button != SDL_BUTTON_LEFT:
+		print('fuck')
+
+	mousePos = e.x, e.y
+
+	print(mousePos)
+
+def handle_event(e):
+    if e.type  == SDL_QUIT:
+        gfw.quit()
+    elif e.type == SDL_KEYDOWN:
+    	KeyDown(e.key)
+    elif e.type == SDL_MOUSEBUTTONDOWN:
+    	Mouse(e)
+
+if __name__=='__main__':
+    gfw.run_main()
